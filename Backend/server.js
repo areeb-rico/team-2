2aba6b6fa1d48adb56e207c93fc6cee7adb5aea8
aaliyaan <aptechschemework@gmail.com>
2026-05-08 16:17:47 +0500

    api by hasnain

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,6 @@ var {MongoClient} = require ('mongodb')
 var express = require ('express')
 var jwt = require ('jsonwebtoken')
 var cors = require ('cors')
-
-
 var mongoserver = new MongoClient ('mongodb://localhost:27017/')
 var apiserver = express()
 apiserver.use(express.json())

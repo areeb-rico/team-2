a88a6ffb6420ee72336c0f207227427422bb462a
arib-rico <arib.rico2008@gmail.com>
2026-05-08 15:53:25 +0500

    hello 1st commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,11 @@
-var a = "Hello Team 2";
-console.log(a);
+var {MongoClient} = require ('mongodb')
+var express = require ('express')
+var jwt = require ('jsonwebtoken')
+var cors = require ('cors')
+
+
+
+var mongoserver = new MongoClient ('mongodb://localhost:27017/')
+var apiserver = express()
+apiserver.use(express.json())
+apiserver.use(cors())
